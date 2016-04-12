f44396ceabb04ed4c928b0e0fd3ee9562d36c5a5
Paulo Simao <paulo.simao@mbr.com.br>
2016-04-11 23:53:29 -0300

    added cr and ur methods

--- cmds.js
+++ cmds.js
@@ -11,19 +11,21 @@ var cmds = function () {
 		n   : 'Creates new task. Param taskname',
 		d   : 'Deletes task. Param ID',
 		c   : 'Marks task as complete. Param ID',
+		cr  : 'Marks tasks as complete. Param regex',
 		u   : 'Marks task as incomplete. Param ID',
+		ur  : 'Marks task as incomplete. Param regex',
 		help: 'This help message'
 	}
 }
 
-cmds.prototype.l    = function () {
+cmds.prototype.l  = function () {
 	for (i in this.tasks) {
 		if (i != '__count') {
 			console.log(`${i}\t[${this.tasks[i].complete ? '*' : ' '}] ${this.tasks[i].desc}`);
 		}
 	}
 }
-cmds.prototype.lr   = function () {
+cmds.prototype.lr = function () {
 	var regex = new RegExp(this.args[3]);
 	for (i in this.tasks) {
 		if (i != '__count') {
@@ -33,10 +35,10 @@ cmds.prototype.lr   = function () {
 		}
 	}
 }
-cmds.prototype.s    = function () {
+cmds.prototype.s  = function () {
 	fs.writeFileSync(FNAME, JSON.stringify(this.tasks));
 }
-cmds.prototype.n    = function () {
+cmds.prototype.n  = function () {
 	var t            = {
 		id  : this.tasks.__count,
 		desc: this.args[3]
@@ -45,19 +47,45 @@ cmds.prototype.n    = function () {
 	this.tasks.__count++;
 	this.s();
 }
-cmds.prototype.d    = function () {
+cmds.prototype.d  = function () {
 	var id = this.args[3];
 	delete this.tasks[id];
 	this.s();
 }
-cmds.prototype.c    = function () {
+cmds.prototype.c  = function () {
+
 	var id                  = this.args[3];
 	this.tasks[id].complete = true;
 	this.s();
 }
+cmds.prototype.cr = function () {
+	var regex = new RegExp(this.args[3]);
+	for (i in this.tasks) {
+		if (i != '__count') {
+			if (regex.test(this.tasks[i].desc)) {
+				this.tasks[i].complete = true
+			}
+		}
+	}
+	this.s();
+}
+
 cmds.prototype.u    = function () {
+	var regex               = new RegExp(this.args[3]);
 	var id                  = this.args[3];
 	this.tasks[id].complete = false;
+	this.s();
+
+}
+cmds.prototype.ur   = function () {
+	var regex = new RegExp(this.args[3]);
+	for (i in this.tasks) {
+		if (i != '__count') {
+			if (regex.test(this.tasks[i].desc)) {
+				this.tasks[i].complete = false
+			}
+		}
+	}
 	this.s();
 
 }
